595b531e7cbec6ed5536348b6e5c7a100e7354ec
Alijamal1314 <ali.jamal.naqvi@gmail.com>
2026-01-14 16:59:10 -0500

    loops example

---
+++ loops.py
@@ -0,0 +1,15 @@
+# For loop example
+print("For loop example:")
+
+for number in range(1, 6):
+    print("Number:", number)
+
+print("-----")
+
+# While loop example
+print("While loop example:")
+
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
