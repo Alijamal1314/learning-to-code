c39eef2546527da5a88046c24d8c4146c826f491
Alijamal1314 <ali.jamal.naqvi@gmail.com>
2026-01-14 22:27:00 -0500

    hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+# File: hello.py
+# Goal: Write and run a simple Python program
+# What I learned:
+# - How to print output to the console
+# - How Python executes code top to bottom
+# - How to run a basic script
+# Next step:
+# - Accept user input instead of hardcoded values
+# - Use variables and functions to expand this program
+
+
 print("Hello, world! This is my first Python file.")
